83497a880ce538874e3ee1f9da4b866fa6172275
tomoose11 <tomwall_84@hotmail.co.uk>
2018-10-23 22:42:33 +0100

    analyzeArray complete

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,10 +1,36 @@
-
-const analyzeArray = (name, arr) => {
-    let res = '';
-    for (let i = 0; i < arr.length; i++) {
-        console.log(`${name}.${i}: ${arr[i]}`);
-        return console.log(`${name}.${i}: ${arr[i]}`);
+const analyzeArray = (collectionName, arr, path = "") => {
+  if (arr.length === 0) {
+    console.log(`${collectionName}.${0}:`);
+  }
+  let result = [];
+  let other = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      path += i;
     }
+    if (i > 0) {
+      path = path.slice(0, -1) + i;
+    }
+    if (Array.isArray(arr[i])) {
+      result = analyzeArray(collectionName, arr[i], path);
+    } else {
+      console.log(`${collectionName}.${path}: ${arr[i]}`);
+      other.push(`${collectionName}${path}${arr[i]}`);
+      //console.log(mySpy(`${collectionName}.${path}: ${arr[i]}`));
+    }
+  }
+  return [...result, ...other];
 };
+
+analyzeArray("Frank", [
+  "Biff",
+  [["pp"], "ttt", "dd", ["gy", ["uu"], "tt"], "bb"],
+  "jan",
+  "ll"
+]);
+
+function mySpy(str) {
+  return str;
+}
 
 module.exports = { analyzeArray };
--- spec/analyzeArray.spec.js
+++ spec/analyzeArray.spec.js
@@ -1,15 +1,32 @@
-const { expect } = require('chai');
-const { sinon } = require('sinon');
-const { analyzeArray } = require('../analyzeArray');
+const { expect } = require("chai");
+const { analyzeArray } = require("../analyzeArray");
+const { assert } = require("chai");
+const sinon = require("sinon");
 
-describe.only('analyzeArray', () => {
-    it('given an empty array returns collection name followed by index 0', () => {
-        expect(analyzeArray('jimbo', [])).to.equal('jimbo.0:');
-    });
-    it('finds path for a single item in a flat array', () => {
-        expect(analyzeArray('jimbo', ['a'])).to.equal('jimbo.0: a');
-    });
-    it('finds the path of any number of items in a flat array', () => {
-        expect(analyzeArray('bobby', ['a', 'b', 'c'])).to.equal(['bobby.0: a', 'bobby.1: b', 'bobby.2: c']);
-    });
+describe("analyzeArray", () => {
+  let spy = sinon.spy(console, "log");
+  it("given an empty array should log a collection name followed by index 0", () => {
+    analyzeArray("Bryan", []);
+    assert(spy.calledWith("Bryan.0:"));
+  });
+  it("finds path for a single item in a flat array", () => {
+    analyzeArray("Frank", ["toy"]);
+    assert(spy.calledWith("Frank.0: toy"));
+  });
+  it("should work for any number of items in a flat array", () => {
+    analyzeArray("bobby", ["a", "b", "c"]);
+    assert(spy.calledWith("bobby.0: a"));
+    assert(spy.calledWith("bobby.1: b"));
+    assert(spy.calledWith("bobby.2: c"));
+  });
+  it("should log all items with correct indexes even when items are nested at several levels", () => {
+    analyzeArray("bobby", ["a", "b", ["c", ["d"], "e"], "f"]);
+    assert(spy.calledWith("bobby.0: a"));
+    assert(spy.calledWith("bobby.1: b"));
+    assert(spy.calledWith("bobby.20: c"));
+    assert(spy.calledWith("bobby.210: d"));
+    assert(spy.calledWith("bobby.22: e"));
+    assert(spy.calledWith("bobby.3: f"));
+    spy.restore();
+  });
 });
