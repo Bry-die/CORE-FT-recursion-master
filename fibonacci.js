b410ce944cb64ba32adcaa185f1a5435b50e6e4b
Bry-die <brydie.caine1@gmail.com>
2018-10-23 09:59:57 +0100

    deepFreeze

--- fibonacci.js
+++ fibonacci.js
@@ -6,4 +6,10 @@ const fibonacci = (n, a = 0, b = 1, res = 1) => {
     return fibonacci(n - 1, a = b, b = res, res);
 };
 
+// const fibonacci = (n) => {
+//     if (n === 1) return 0;
+//     if (n === 2) return 1;
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// };
+
 module.exports = { fibonacci };
